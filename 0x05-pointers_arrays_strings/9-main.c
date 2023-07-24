d9b633f6ba7b4e31204f35946e819415a9902076
drken24 <kenomotto45@gmail.com.com>
2023-07-24 11:45:48 -0700

    l

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -1,18 +1,18 @@
-/**
- * _strcpy - a function that copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest.
- *
- * @dest: A pointer to destination of string
- * @src: A pointer to source string to copy from
- *
- * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	char *aux = dest;
+#include "main.h"
+#include <stdio.h>
 
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char s1[98];
+  char *p;
+
+  p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+  printf("%s", s1);
+  printf("%s", p);
+  return (0);
 }
